46e7e77e749293aa4354cc45e9a513f4f5af6925
mtraina <matteo.traina.mail@gmail.com>
2016-04-26 16:56:18 +0200

    ws call to speech to text service with external conf

--- app/services/SpeechService.scala
+++ app/services/SpeechService.scala
@@ -1,17 +1,37 @@
 package services
 
+import java.io.File
 import javax.inject.Inject
 
-import play.api.libs.ws.WSClient
+import play.api.Configuration
+import play.api.libs.ws.WSAuthScheme.BASIC
+import play.api.libs.ws.{WSClient, WSResponse}
+
+import scala.concurrent.{ExecutionContext, Future}
 
 trait SpeechService {
   def speechToText(): String
 }
 
-class SpeechServiceImpl @Inject()(ws: WSClient) extends SpeechService {
+class SpeechServiceImpl @Inject()(val ws: WSClient,
+                                  implicit val context: ExecutionContext,
+                                  val configuration: Configuration) extends SpeechService {
+
   override def speechToText(): String = {
-    //val url = "https://stream.watsonplatform.net/speech-to-text/api/v1/recognize?timestamps=true&word_alternatives_threshold=0.9"
-    //ws.url(url)
+    val url = configuration.getString("speech.converter.service.url")
+    val username = configuration.getString("speech.converter.service.username")
+    val password = configuration.getString("speech.converter.service.password")
+
+    val response: Future[WSResponse] = ws.url(url.get)
+      .withAuth(username.get, password.get, BASIC)
+      .withHeaders("content-type" -> "audio/wav")
+        .post(new File("data/myRecording01.wav"))
+
+    //val r = Await.result(response, Duration.Inf)
+
+    response.map {
+      r => print(r.json)
+    }
     "text"
   }
 }
