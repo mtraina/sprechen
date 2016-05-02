9c0e7994c31fe01868d3c51ad31b6798b2ff7d1d
mtraina <matteo.traina.mail@gmail.com>
2016-05-02 16:31:15 +0200

    jquery removed and cleanup

--- app/assets/js/app.jsx
+++ app/assets/js/app.jsx
@@ -2,47 +2,37 @@ import React from "react";
 import ReactDOM from "react-dom";
 import 'es6-promise';
 import 'whatwg-fetch';
-import $ from "jquery";
 
 class CommentBox extends React.Component {
+
   previewFile(event) {
     console.log("previewFile...in");
 
-    //let audio;
-    //let preview = document.querySelector('img');
-    let file    = document.querySelector('input[type=file]').files[0];
-    let reader  = new FileReader();
+    const file = document.querySelector("#audio").files[0];
+    const reader = new FileReader();
 
-    console.log("previewFile...after let setup");
-
-    //reader.onload = (upload) => this.getData(upload);
     reader.onloadend = (upload) => {
-      console.log(upload);
-
-      const file_data = $("#audio").prop("files")[0];
       const data = new FormData();
-      data.append("speech", file_data);
+      data.append("speech", file);
 
       fetch("http://localhost:9000/recognize", {
-        method: "POST",
-        body: data
-      })
-      .then(function(response){
-        console.log("response: ", response);
-        //console.log("json:", response.json());
-        return response.json();
-      })
-      .then(function(json){
-        console.log("json: ", json);
-      })
-      .catch(function(error) {
-        console.log("Request failed", error );
-      });
-  };
+          method: "POST",
+          body: data
+        })
+        .then(function(response){
+          return response.json();
+        })
+        .then(function(json){
+          console.log("json: ", json);
+        })
+        .catch(function(error) {
+          console.log("Request failed", error);
+        });
+    };
 
     if (file) {
-      console.log("previewFile...is file");
-      reader.readAsDataURL(file);
+        console.log("previewFile...is file");
+        reader.readAsDataURL(file);
     }
   }
 
