dc634aca589559e724473b0f0a4a04c3cc9bbce7
mtraina <matteo.traina.mail@gmail.com>
2016-05-03 16:04:33 +0200

    key added to the list element to remove the warning

--- app/assets/js/list.jsx
+++ app/assets/js/list.jsx
@@ -9,7 +9,7 @@ class ListItem extends React.Component {
 class List extends React.Component {
   render() {
       return <ul>
-        {this.props.data.map(d => <ListItem data={d}/>)}
+        {this.props.data.map((d, i) => <ListItem key={i} data={d}/>)}
       </ul>;
   }
 }
