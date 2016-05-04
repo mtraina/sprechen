80c8c9b1204c17b6a40b1d82cd7f29716b34962a
mtraina <matteo.traina.mail@gmail.com>
2016-05-04 18:06:43 +0200

    recorder start/stop buttons added

--- app/assets/js/recorder.jsx
+++ app/assets/js/recorder.jsx
@@ -1,9 +1,38 @@
 import React from "react";
 
 class Recorder extends React.Component {
-    render(){
-      return <h1>Recorder</h1>;
-    }
+  constructor(props){
+    super(props);
+    //this.audioContext = new AudioContext;
+    //let input = audioContext.createMediaStreamSource(stream);
+    //this.recorder = new Recorder(input);
+  }
+
+  componentDidMount(){
+    document.querySelector("#stop").disabled = true;
+  }
+
+  startRecording() {
+    //recorder && recorder.record();
+    document.querySelector("#start").disabled = true;
+    document.querySelector("#stop").disabled = false;
+    //button.nextElementSibling.disabled = false;
+    console.log('Start recording...');
+  }
+
+  stopRecording(){
+    console.log('Stop recording...');
+
+    document.querySelector("#stop").disabled = true;
+    document.querySelector("#start").disabled = false;
+  }
+
+  render(){
+    return <div>
+      <button id="start" onClick={this.startRecording}>record</button>
+      <button id="stop" onClick={this.stopRecording}>stop</button>
+    </div>;
+  }
 }
 
 export default Recorder;
