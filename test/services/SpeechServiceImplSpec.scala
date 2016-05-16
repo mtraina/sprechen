6e652ae5b30d61e347d9e358296baf4b5c5e0f19
mtraina <matteo.traina.mail@gmail.com>
2016-05-16 21:20:17 +0200

    spec fixed

--- test/services/SpeechServiceImplSpec.scala
+++ test/services/SpeechServiceImplSpec.scala
@@ -5,6 +5,7 @@ import java.io.File
 import org.mockito.BDDMockito._
 import org.scalatest.mock.MockitoSugar
 import org.scalatest.{FlatSpec, Matchers}
+import persistence.SpeechDao
 import play.api.libs.ws.WSResponse
 import ws.SpeechWSClient
 
@@ -15,8 +16,9 @@ class SpeechServiceImplSpec extends FlatSpec with Matchers with MockitoSugar {
   val r = mock[WSResponse]
   val response = Future.successful(r)
   val client = mock[SpeechWSClient]
+  val dao = mock[SpeechDao]
   given(client.post(f)) willReturn response
-  val speechService = new SpeechServiceImpl(client)
+  val speechService = new SpeechServiceImpl(client, dao)
 
   "A speech service" should "return the text related to the speech" in {
     speechService.speechToText(f) shouldBe response
