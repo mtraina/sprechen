1f4dc8a9eb555def406c1e08bd2b3fcfa3a3509c
mtraina <matteo.traina.mail@gmail.com>
2016-05-19 21:20:10 +0200

    embedded mongo in spec I

--- build.sbt
+++ build.sbt
@@ -10,8 +10,10 @@ libraryDependencies ++= Seq(
   ws,
   "org.reactivemongo" %% "play2-reactivemongo" % "0.11.11",
   "org.scalatestplus.play" %% "scalatestplus-play" % "1.5.1" % Test,
-  "org.mockito" % "mockito-all" % "1.10.19" % "test",
-  specs2 % Test
+  "org.mockito" % "mockito-all" % "1.10.19" % Test,
+  specs2 % Test,
+  "de.flapdoodle.embed" % "de.flapdoodle.embed.mongo" % "1.50.3"
 )
 
 resolvers += "scalaz-bintray" at "http://dl.bintray.com/scalaz/releases"
+javaOptions in Test += "-Dconfig.file=conf/application.test.conf"
--- test/persistence/SpeechDaoSpec.scala
+++ test/persistence/SpeechDaoSpec.scala
@@ -1,26 +1,52 @@
 package persistence
 
-import org.specs2.concurrent.{ExecutionEnv => EE}
+import com.github.athieriot.EmbedConnection
+import de.flapdoodle.embed.mongo.MongodStarter
+import de.flapdoodle.embed.mongo.config.{Net, MongodConfigBuilder}
+import de.flapdoodle.embed.mongo.distribution.Version
+import de.flapdoodle.embed.process.runtime.Network
+import org.specs2.specification.BeforeAfterAll
 import play.api.test.{PlaySpecification, WithApplication}
+import reactivemongo.api.MongoDriver
 
-import scala.concurrent.{Await, ExecutionContext}
 import scala.concurrent.duration._
+import scala.concurrent.{Await, ExecutionContext}
 
-class SpeechDaoSpec extends PlaySpecification {
+class SpeechDaoSpec extends PlaySpecification with BeforeAfterAll {
+
+  override def beforeAll(): Unit = {
+    import scala.concurrent.ExecutionContext.Implicits.global
+
+    //Override this method to personalize testing port
+    def embedConnectionPort: Int = 12345
+
+    //Override this method to personalize MongoDB version
+    def embedMongoDBVersion: Version.Main = Version.Main.PRODUCTION
+    lazy val network = new Net(embedConnectionPort, Network.localhostIsIPv6)
+
+    lazy val mongodConfig = new MongodConfigBuilder()
+      .version(embedMongoDBVersion)
+      .net(network)
+      .build
+    lazy val runtime = MongodStarter.getDefaultInstance
+    lazy val mongodExecutable = runtime.prepare(mongodConfig)
+    mongodExecutable.start
+  }
+
+  override def afterAll(): Unit = {
+    //mongodExecutable.start
+  }
 
   "SpeechDao" should {
     "get a value" in new WithApplication(){
       implicit val context = app.injector.instanceOf[ExecutionContext]
       val speechDao = app.injector.instanceOf[SpeechDao]
-      //speechDao must be (null)
-      //val result = speechDao.create(Speech(transcript = "cheers2"))
-
-      //speechDao.find().map(s => s.length) must beEqualTo(1).awaitFor(2 seconds)
       val timeout = 5 seconds
       val result = Await.result(speechDao.find(), timeout)
 
       result.length must beEqualTo(3)
     }
   }
+
 
 }
