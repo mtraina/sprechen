4220c38eaa80c1ef32941911d2c7c253a5acdda1
mtraina <matteo.traina.mail@gmail.com>
2016-05-28 18:03:56 +0200

    clean up

--- app/assets/js/App.jsx
+++ app/assets/js/App.jsx
@@ -1,16 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
 import "es6-promise";
 import "whatwg-fetch";
+import Routes from './Routes.jsx'
 
-//import Router from './Router.js'
-
-import LoginForm from './LoginForm.jsx';
-import CommentBox from './CommentBox.jsx';
-import { Router, Route, hashHistory } from 'react-router';
-
-ReactDOM.render((
-  <Router history={hashHistory}>
-    <Route path="/" component={CommentBox} />
-    <Route path="/login" component={LoginForm} />
-  </Router>), document.getElementById('content'));
+render(<Routes/>, document.getElementById('content'));
