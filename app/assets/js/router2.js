ff5e9d37bab3e52efde8aca319a36c2784371176
mtraina <matteo.traina.mail@gmail.com>
2016-05-28 17:30:18 +0200

    router renamed lowercase I

--- app/assets/js/router2.js
+++ app/assets/js/router2.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { Router, Route, browserHistory, IndexRoute } from 'react-router';
+import { Router, Route, hashHistory } from 'react-router';
 
 import CommentBox from './CommentBox.jsx';
 import LoginForm from './LoginForm.jsx';
 
 export default (
-  <Router history={browserHistory}>
+  <Router history={hashHistory}>
     <Route path="/" component={CommentBox} />
     <Route path="/login" component={LoginForm} />
-  </Router>
-);
+  </Router>)
