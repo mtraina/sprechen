cd57f2fd0e31d91d93634e7d895ecf97150e5f68
mtraina <matteo.traina.mail@gmail.com>
2016-10-18 22:30:25 +0200

    data table values adaptaton I

--- app/assets/javascripts/dictionary/ResultTable.jsx
+++ app/assets/javascripts/dictionary/ResultTable.jsx
@@ -6,28 +6,22 @@ export default class ResultTable extends React.Component {
   constructor(props){
       super(props);
       this.state = {
-        rows : [{"id":1,"first_name":"William","last_name":"Elliott","email":"welliott0@wisc.edu",
-             "country":"Argentina","ip_address":"247.180.226.89"},
-              {"id":2,"first_name":"Carl","last_name":"Ross","email":"cross1@mlb.com",
-             "country":"South Africa","ip_address":"27.146.70.36"},
-              {"id":3,"first_name":"Jeremy","last_name":"Scott","email":"jscott2@cbsnews.com",
-             "country":"Colombia","ip_address":"103.52.74.225"}]
+        rows : [{"text":"weather","translations":["Wetter", "Another"]}]
       };
   }
 
   render() {
-      return <Layout><Table
-        height={40+((this.state.rows.length+1) * 30)}
-        width={1150}
-        rowsCount={this.state.rows.length}
-        rowHeight={30}
-        headerHeight={30}
-        rowGetter={function(rowIndex) {return this.state.rows[rowIndex]; }.bind(this)}>
-        <Column dataKey="id" width={50} label="Id" />
-        <Column dataKey="first_name" width={200} label="First Name" />
-        <Column dataKey="last_name" width={200} label="Last Name" />
-        <Column dataKey="email" width={400} label="e-mail" />
-        <Column dataKey="country" width={300} label="Country" />
-      </Table></Layout>;
+      return <Layout>
+        <Table height={40+((this.state.rows.length+1) * 30)}
+          width={1150}
+          rowsCount={this.state.rows.length}
+          rowHeight={30}
+          headerHeight={30}
+          rowGetter={function(rowIndex) {return this.state.rows[rowIndex]; }.bind(this)}>
+          <Column dataKey="text" width={300} label="Text" />
+          <Column dataKey="translations" width={850} label="Translations"
+            cellRenderer={cellData => cellData.join()}/>
+        </Table>
+      </Layout>;
     }
 }
