69191bdbd8bf8809f36808bd829f68dcfadcb454
mtraina <matteo.traina.mail@gmail.com>
2016-10-27 22:30:05 +0200

    service client refactoring: "private" request method added

--- app/assets/javascripts/client/ServiceClient.js
+++ app/assets/javascripts/client/ServiceClient.js
@@ -1,11 +1,18 @@
 import { hashHistory } from 'react-router';
 import cookie from 'react-cookie';
 
+const executeRequest = Symbol();
+
 class ServiceClient {
 
   get(url, callback){
-    fetch("/speeches", {
-        method: "GET",
+    this[executeRequest](url, "GET", callback);
+  }
+
+  post(url, data, callback){
+    fetch(url, {
+        method: "POST",
+        body: data,
         headers: {
           "X-AUTH-TOKEN": cookie.load("AUTH_TOKEN")
         }
@@ -20,6 +27,25 @@ class ServiceClient {
       })
       .catch(error => console.log("Request failed", error))
   }
+
+  [executeRequest](url, method, callback){
+    fetch(url, {
+        method: method,
+        headers: {
+          "X-AUTH-TOKEN": cookie.load("AUTH_TOKEN")
+        }
+      })
+      .then(r => {
+        if(r.status == 401) hashHistory.push('/login');
+        else return r.json();
+      })
+      .then(json => {
+        console.log("json: ", json);
+        callback(json);
+      })
+      .catch(error => console.log("Request failed", error))
+  }
+
 }
 
 export let serviceClient = new ServiceClient();
